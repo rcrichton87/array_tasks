d3f1e9f307f6586095b2b429e038d7265a61a864
Ross Crichton <rcrichton87@googlemail.com>
2017-04-05 16:32:02 +0100

    concat test passed

---
+++ array_tasks.js
@@ -0,0 +1,40 @@
+var arrayTasks = {
+
+	concat: function (arr1, arr2) {
+		arr2.forEach(function(element){
+			arr1.push(element);
+		})
+		return arr1;
+	},
+
+	// insertAt: function (arr, itemToAdd, index) {
+		
+	// },
+
+	// square: function (arr) {
+		
+	// },
+
+	// sum: function (arr) {
+		
+	// },
+
+	// findDuplicates: function (arr) {
+		
+	// },
+
+	// removeAndClone: function (arr, valueToRemove) {
+		
+	// },
+
+	// findIndexesOf: function (arr, itemToFind) {
+		
+	// },
+
+	// sumOfAllEvenNumbersSquared: function (arr) {
+		
+	// }
+
+}
+
+module.exports = arrayTasks
